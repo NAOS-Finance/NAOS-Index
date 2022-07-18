36fde472e8fcca3389185bdf969512ab15fba82c
x64x48 <x64x48@protonmail.com>
2022-07-18 15:57:07 +0800

    Remove scripts/test

--- scripts/blockchain_scripts/setUpForTesting.ts
+++ scripts/blockchain_scripts/setUpForTesting.ts
@@ -1,6 +1,4 @@
-// import {NON_US_UID_TYPES, US_UID_TYPES} from "@goldfinch-eng/autotasks/unique-identity-signer/utils"
-// import {JsonRpcSigner} from "ethers/lib"
-import {assertIsString, assertNonNullable, findEnvLocal} from "./utils"
+import {assertIsString, assertNonNullable, findEnvLocal, NON_US_UID_TYPES, US_UID_TYPES} from "./utils"
 import BigNumber from "bignumber.js"
 import BN from "bn.js"
 import dotenv from "dotenv"
@@ -35,20 +33,15 @@ import {
 } from "./deployHelpers"
 import {Logger} from "../blockchain_scripts/types"
 import {time} from "@openzeppelin/test-helpers"
-// import {toEthers} from "../test/testHelpers"
 import {
   CreditLine,
   Go,
   GoldfinchConfig,
   GoldfinchFactory,
   SeniorPool,
-  // StakingRewards,
   TranchedPool,
   UniqueIdentity,
 } from "../../types/contracts/protocol/core"
-import {
-  BackerRewards,
-} from "../../types/contracts/rewards"
 import {
   Borrower,
 } from "../../types/contracts/protocol/periphery"
@@ -62,24 +55,6 @@ import {overrideUsdcDomainSeparator} from "./mainnetForkingHelpers"
 
 // dotenv.config({path: findEnvLocal()})
 dotenv.config()
-
-const US_COUNTRY_CODE = "US"
-export const NON_US_INDIVIDUAL_ID_TYPE_0 = 0 // non-US individual
-export const US_ACCREDITED_INDIVIDUAL_ID_TYPE_1 = 1 // US accredited individual
-export const US_NON_ACCREDITED_INDIVIDUAL_ID_TYPE_2 = 2 // US non accredited individual
-export const US_ENTITY_ID_TYPE_3 = 3 // US entity
-export const NON_US_ENTITY_ID_TYPE_4 = 4 // non-US entity
-export const US_UID_TYPES = [
-  US_ACCREDITED_INDIVIDUAL_ID_TYPE_1,
-  US_NON_ACCREDITED_INDIVIDUAL_ID_TYPE_2,
-  US_ENTITY_ID_TYPE_3,
-]
-export const NON_US_UID_TYPES = [NON_US_INDIVIDUAL_ID_TYPE_0, NON_US_ENTITY_ID_TYPE_4]
-
-export const BACKER_REWARDS_MAX_INTEREST_DOLLARS_ELIGIBLE = new BigNumber(100_000_000)
-  .multipliedBy(new BigNumber(1e18))
-  .toString(10)
-export const BACKER_REWARDS_PERCENT_OF_TOTAL_GFI = 2
 
 const SECONDS_PER_DAY = new BN(86400)
 
@@ -135,8 +110,8 @@ export async function setUpForTesting(hre: HardhatRuntimeEnvironment, {overrideA
   const {gf_deployer} = await getNamedAccounts()
   const protocol_owner = await getProtocolOwner()
   const deployer = new ContractDeployer(logger, hre)
-  // assertIsString(protocol_owner)
-  // assertIsString(gf_deployer)
+  assertIsString(protocol_owner)
+  assertIsString(gf_deployer)
   const protocolOwnerSigner = ethers.provider.getSigner(protocol_owner)
   
   const chainId = await getChainId()
@@ -147,11 +122,8 @@ export async function setUpForTesting(hre: HardhatRuntimeEnvironment, {overrideA
   // which sets this var to the upgraded version of fidu
   // let fidu = await getDeployedAsEthersContract(getOrNull, "Fidu")
   let config = await getDeployedAsEthersContract<GoldfinchConfig>(getOrNull, "GoldfinchConfig")
-  // assertNonNullable(config)
+  assertNonNullable(config)
   const goldfinchFactory = await getDeployedAsEthersContract<GoldfinchFactory>(getOrNull, "GoldfinchFactory")
-  if (process.env.TEST_USERS) {
-    throw new Error("`TEST_USERS` is deprecated. Use `TEST_USER` instead.")
-  }
   const borrower = overrideAddress || process.env.TEST_USER || protocol_owner
   const requestFromClient = !!overrideAddress
 
@@ -629,7 +601,6 @@ async function getDeployedAsEthersContractOrNull<T>(
   // }
   const deployed = await getContract<T, any>(name, ETHERS_CONTRACT_PROVIDER)
   if (deployed) {
-    // return await toEthers<T>(deployed as Parameters<typeof toEthers>[0])
     return deployed
   } else {
     return null
@@ -693,7 +664,7 @@ async function createPoolForBorrower({
   const lastEventArgs = getLastEventArgs(result)
   const poolAddress = lastEventArgs[0]
   const poolContract = await getDeployedAsEthersContract<TranchedPool>(getOrNull, "TranchedPool")
-  // assertNonNullable(poolContract)
+  assertNonNullable(poolContract)
   const pool = poolContract.attach(poolAddress).connect(underwriterSigner)
 
   logger(`Created a Pool ${poolAddress} for the borrower ${borrower}`)
