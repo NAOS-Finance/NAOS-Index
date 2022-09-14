1c9a9dbdf76a22070f4b7ba1f6a24bcd9548d009
x64x48 <x64x48@protonmail.com>
2022-09-14 19:34:14 +0800

    Update RWA test

--- test/RWA.test.ts
+++ test/RWA.test.ts
@@ -5,40 +5,38 @@ import {expect, bigVal, expectAction, bnToHex} from "./testHelpers"
 import {OWNER_ROLE} from "../scripts/blockchain_scripts/deployHelpers"
 import {CONFIG_KEYS} from "../scripts/blockchain_scripts/configKeys"
 import {deployBaseFixture} from "./util/fixtures"
-const GoldfinchConfig = artifacts.require("GoldfinchConfig")
-const Fidu = artifacts.require("Fidu")
+const NAOSConfig = artifacts.require("NAOSConfig")
+const RWA = artifacts.require("RWA")
 
-describe("Fidu", () => {
+describe("RWA", () => {
   const testSetup = deployments.createFixture(async ({deployments, getNamedAccounts}) => {
-    // Just to be crystal clear
     const {protocol_owner} = await getNamedAccounts()
     owner = protocol_owner
 
-    const {fidu, goldfinchConfig} = await deployBaseFixture()
-
-    return {fidu, goldfinchConfig}
+    const {rwa, naosConfig, indexPool} = await deployBaseFixture()
+    return {rwa, naosConfig, indexPool}
   })
 
-  let owner, person2, goldfinchConfig, fidu
+  let owner, person2, naosConfig, rwa, indexPool
   beforeEach(async () => {
     // Pull in our unlocked accounts
     // eslint-disable-next-line @typescript-eslint/no-extra-semi
     ;[owner, person2] = await web3.eth.getAccounts()
-    ;({fidu, goldfinchConfig} = await testSetup())
+    ;({rwa, naosConfig, indexPool} = await testSetup())
   })
 
   describe("Initialization", async () => {
     // beforeEach(async () => {
-    //   goldfinchConfig = await GoldfinchConfig.new({from: owner})
-    //   await goldfinchConfig.initialize(owner)
+    //   naosConfig = await NAOSConfig.new({from: owner})
+    //   await naosConfig.initialize(owner)
 
-    //   fidu = await Fidu.new({from: owner})
-    //   await fidu.__initialize__(owner, "Fidu", "FIDU", goldfinchConfig.address)
+    //   rwa = await RWA.new({from: owner})
+    //   await rwa.__initialize__(owner, "RWA", "RWA", naosConfig.address)
     // })
 
     describe("initialization", async () => {
       it("should not allow it to be called twice", async () => {
-        return expect(fidu.__initialize__(person2, "Fidu", "FIDU", goldfinchConfig.address)).to.be.rejectedWith(
+        return expect(rwa.__initialize__(person2, "RWA", "FIDU", naosConfig.address)).to.be.rejectedWith(
           /has already been initialized/
         )
       })
@@ -46,26 +44,26 @@ describe("Fidu", () => {
 
     describe("ownership", async () => {
       it("should be owned by the owner", async () => {
-        expect(await fidu.hasRole(OWNER_ROLE, owner)).to.be.true
+        expect(await rwa.hasRole(OWNER_ROLE, owner)).to.be.true
       })
     })
   })
 
-  describe("updateGoldfinchConfig", () => {
+  describe("updateNAOSConfig", () => {
     describe("setting it", async () => {
       it("should allow the owner to set it", async () => {
-        await goldfinchConfig.setAddress(CONFIG_KEYS.GoldfinchConfig, person2)
-        return expectAction(() => fidu.updateGoldfinchConfig({from: owner})).toChange([
-          [() => fidu.config(), {to: person2, bignumber: false}],
+        await naosConfig.setAddress(CONFIG_KEYS.NAOSConfig, person2)
+        return expectAction(() => rwa.updateNAOSConfig({from: owner})).toChange([
+          [() => rwa.config(), {to: person2, bignumber: false}],
         ])
       })
 
       it("emits an event", async () => {
-        const newConfig = await deployments.deploy("GoldfinchConfig", {from: owner})
-        await goldfinchConfig.setAddress(CONFIG_KEYS.GoldfinchConfig, newConfig.address, {from: owner})
-        const tx = await fidu.updateGoldfinchConfig()
+        const newConfig = await deployments.deploy("NAOSConfig", {from: owner})
+        await naosConfig.setAddress(CONFIG_KEYS.NAOSConfig, newConfig.address, {from: owner})
+        const tx = await rwa.updateNAOSConfig()
 
-        // expectEvent(tx, "GoldfinchConfigUpdated", {
+        // expectEvent(tx, "NAOSConfigUpdated", {
         //   who: owner,
         //   configAddress: newConfig.address,
         // })
@@ -73,7 +71,7 @@ describe("Fidu", () => {
 
       it("should disallow non-owner to set", async () => {
         const signer = await ethers.getSigner(person2 as string)
-        return expect(fidu.connect(signer).updateGoldfinchConfig()).to.be.rejectedWith(/Must have minter role/)
+        return expect(rwa.connect(signer).updateNAOSConfig()).to.be.rejectedWith(/Must have minter role/)
       })
     })
   })
@@ -83,15 +81,15 @@ describe("Fidu", () => {
       // Use the full deployment so we have a pool, and the
       // mintTo function doesn't fail early on the assets/liabilites check
       const deployments = await testSetup()
-      fidu = deployments.fidu
+      rwa = deployments.rwa
     })
     it("should allow the minter to call it", async () => {
       const signer = await ethers.getSigner(owner as string)
-      return expect(fidu.connect(signer).mintTo(person2, bnToHex(bigVal(1)))).to.be.fulfilled
+      return expect(rwa.connect(signer).mintTo(person2, bnToHex(bigVal(1)))).to.be.fulfilled
     })
     it("should not allow anyone else to call it", async () => {
       const signer = await ethers.getSigner(person2 as string)
-      return expect(fidu.connect(signer).mintTo(person2, bnToHex(bigVal(1)))).to.be.rejectedWith(/minter role/)
+      return expect(rwa.connect(signer).mintTo(person2, bnToHex(bigVal(1)))).to.be.rejectedWith(/minter role/)
     })
   })
 
@@ -101,17 +99,17 @@ describe("Fidu", () => {
       // burnFrom function doesn't fail early on the assets/liabilites check
       const deployments = await testSetup()
       const signer = await ethers.getSigner(owner as string)
-      fidu = deployments.fidu
-      await fidu.connect(signer).mintTo(person2, bnToHex(bigVal(1)))
+      rwa = deployments.rwa
+      await rwa.connect(signer).mintTo(person2, bnToHex(bigVal(1)))
     })
 
     it("should allow the minter to call it", async () => {
       const signer = await ethers.getSigner(owner as string)
-      return expect(fidu.connect(signer).burnFrom(person2, bnToHex(bigVal(1)))).to.be.fulfilled
+      return expect(rwa.connect(signer).burnFrom(person2, bnToHex(bigVal(1)))).to.be.fulfilled
     })
     it("should not allow anyone else to call it", async () => {
       const signer = await ethers.getSigner(person2 as string)
-      return expect(fidu.connect(signer).burnFrom(person2, bnToHex(bigVal(1)))).to.be.rejectedWith(/minter role/)
+      return expect(rwa.connect(signer).burnFrom(person2, bnToHex(bigVal(1)))).to.be.rejectedWith(/minter role/)
     })
   })
 })
